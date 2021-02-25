7df51fa6ed212991b71dd67c117964797576ee5d
Your Name <your_email@whatever.com>
2021-02-25 20:18:38 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import {View} from "react-native";
-import Button from ".components/Button";
+import { View, Text } from "react-native";
+import Button from ".src/components/Button";
 
-class App extends React.Component{
-  render(){
-      return (
-        <View
-          style = {{
-            flex:1,
-            justifyContent: "center",
-            alignContent: "center"
-          }}
-          >
-          <Button/>
-        </View>
+class App extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignContent: "center"
+        }}
+      >
+        <Button />
+      </View>
     )
   }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 
-class Button extends React.Component{
-    render(){
-        return(
+class Button extends React.Component {
+    render() {
+        return (
             <TouchableOpacity>
                 <Text>Press me</Text>
             </TouchableOpacity>
