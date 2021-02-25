eb941041091fd3769aeb076b4cbf7b793a2658be
Your Name <your_email@whatever.com>
2021-02-25 18:17:18 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {View, Text} from "react-native";
+
+class App extends React.Component{
+  render(){
+      return(
+        <View
+          style = {{
+            flex:1,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Text> Hello World</Text>
+        </View>
+     )
+  }
+}
+export default App;
