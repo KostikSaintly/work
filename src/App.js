a1f028dfa56fe27faef35b48e8ff9314e8e62711
Kostya Saintly <timim.kos@yandex.ru>
2021-03-04 13:43:32 +0300

    dz ne sdelano

--- src/App.js
+++ src/App.js
@@ -1,64 +1,70 @@
-import React from "react";
-import { View, Text, TextInput, Image, StyleSheet } from "react-native";
-import ButtonNext from "./components/Button";
-import ButtonPrevious from "./components/ButtonPrevious";
-const ImageURI = [
-  'https://reactnative.dev/img/tiny_logo.png',
-  'https://i.pinimg.com/736x/cd/fd/fe/cdfdfe1fea18045b9b41f661aad4f655--corvette-c-glass.jpg',
-  'https://www.smalljp.ru/var/cars_big/288042-tm1456276875.jpg',
-  'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/finnish-night-sky-joni-niemela-finland-2.jpg',
-  'https://ic.pics.livejournal.com/imhotype/24051864/433166/433166_original.png'
-];
+import React from 'react';
+import { FlatList, Image, View, Text } from 'react-native';
 
 class App extends React.Component {
   state = {
-    uri: ImageURI[0],
-    count: 0,
-    nextShow: true,
-    previousShow: true
-  }
-
-  nextImage = () => {
-    this.setState({
-      uri: ImageURI[this.state.count + 1]
-    });
-    this.state.count = this.state.count + 1;
-
-    if (this.state.count == 4) {
-      this.state.nextShow = false;
-    }
-    else {
-      this.state.nextShow = true;
-    }
-  }
-  previousImage = () => {
-    this.setState({
-      uri: ImageURI[this.state.count - 1]
-    });
-    this.state.count = this.state.count - 1;
-
-    if (this.state.count != 0) {
-      this.state.previousShow = true;
-    }
-    else {
-      this.state.previousShow = false;
-    }
-  }
+    images: [
+      {
+        key: 0,
+        url: 'https://reactnative.dev/img/tiny_logo.png',
+        title: 'rn'
+      },
+      {
+        key: 1,
+        url: 'https://i.pinimg.com/736x/cd/fd/fe/cdfdfe1fea18045b9b41f661aad4f655--corvette-c-glass.jpg',
+        title: 'car'
+      },
+      {
+        key: 2,
+        url: 'https://www.smalljp.ru/var/cars_big/288042-tm1456276875.jpg',
+        title: 'moto'
+      },
+      {
+        key: 3,
+        url: 'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/finnish-night-sky-joni-niemela-finland-2.jpg',
+        title: 'night'
+      },
+      {
+        key: 4,
+        url: 'https://ic.pics.livejournal.com/imhotype/24051864/433166/433166_original.png',
+        title: 'pepe'
+      }
+    ]
+  };
 
 
   render() {
+    const { images } = this.state;
+
     return (
-      <View>
-        <Image
-          source={{ uri: this.state.uri }}
-          style={{
-            width: 250,
-            height: 250,
-          }}
-        />
-        {this.state.previousShow && <ButtonPrevious previousImage={this.previousImage} />}
-        {this.state.nextShow && <ButtonNext nextImage={this.nextImage} />}
-      </View>
+      <FlatList
+        data={images}
+        renderItem={({ item }) => (
+          <View>
+            <Image
+              style={{
+                width: 400,
+                height: 400,
+                justifyContent: 'center',
+                alignItems: 'center',
+                flex: 1
+              }}
+              source={{
+                uri: item.url
+              }}
+            />
+            <Text style={{
+              fontSize: 40,
+              textAlign: "center",
+              paddingBottom: 10
+            }}
+            >
+              {item.title}
+            </Text>
+          </View>
+        )}
+      />
+
     );
   }
 }
